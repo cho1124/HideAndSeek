ba33c627d6c9f9e5e3959af21f114af3fc8265a1
한유진 <bittersweeety@gmail.com>
2024-08-11 20:47:30 +0900

    [Test]

--- Assets/3.Script/Move.cs
+++ Assets/3.Script/Move.cs
@@ -6,6 +6,7 @@ public class Move : MonoBehaviour
 {
 
     public float Speed = 5f;
+    public Camera mainCamera; // 메인 카메라를 참조하기 위한 변수
 
     // Update is called once per frame
     void Update()
@@ -14,7 +15,20 @@ public class Move : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal") * Speed*Time.deltaTime;
         float moveZ = Input.GetAxis("Vertical") * Speed*Time.deltaTime;
 
-        transform.Translate(moveX,0,moveZ);
-        
+        // 이동 적용
+        transform.Translate(moveX, 0, moveZ);
+
+        // 마우스 위치를 월드 좌표로 변환
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit)) {
+            // 마우스가 가리키는 위치의 XZ 평면 좌표만 사용
+            Vector3 targetPosition = hit.point;
+            targetPosition.y = transform.position.y; // Y축 고정
+
+            // 플레이어가 마우스를 바라보도록 회전 (XZ 평면만)
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime);
+        }
     }
 }
