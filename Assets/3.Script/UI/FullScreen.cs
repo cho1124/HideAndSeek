f31f42f7490ee58c88e0c72bf2ac2beb21bbf542
JeongDongKyun <141615537+JeongDongKyun@users.noreply.github.com>
2024-08-19 00:04:07 +0900

    UI fixed

--- Assets/3.Script/UI/FullScreen.cs
+++ Assets/3.Script/UI/FullScreen.cs
@@ -5,13 +5,13 @@ using TMPro;
 
 public class FullScreen : MonoBehaviour
 {
-    private TextMeshProUGUI on_off_text;
+    private TextMeshPro on_off_text;
 
     private bool on_off_tr = true;
 
     private void Awake()
     {
-        on_off_text = this.GetComponentInChildren<TextMeshProUGUI>();
+        on_off_text = this.GetComponentInChildren<TextMeshPro>();
     }
 
     private void Start()
@@ -25,19 +25,19 @@ public class FullScreen : MonoBehaviour
 
         if (on_off_tr)
         {
-            aaaaaaaaaaaaaaaSetResolution(1920, 1080, true);
+            SetResolution(1920, 1080, true);
 
             on_off_text.text = "ON";
         }
         else
         {
-            aaaaaaaaaaaaaaaSetResolution(1280, 720, false);
+            SetResolution(1280, 720, false);
 
             on_off_text.text = "OFF";
         }
     }
 
-    public void aaaaaaaaaaaaaaaSetResolution(int width, int height, bool fullscreen)
+    private void SetResolution(int width, int height, bool fullscreen)
     {
         Screen.SetResolution(width, height, fullscreen);
     }
