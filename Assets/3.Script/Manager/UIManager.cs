41a50aa9736b16de4db4655ec44fb32f244445d9
cho1124 <122534567+cho1124@users.noreply.github.com>
2024-08-18 20:41:02 +0900

    [Update] Some Map Update

--- Assets/3.Script/Manager/UIManager.cs
+++ Assets/3.Script/Manager/UIManager.cs
@@ -9,55 +9,11 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI hp_text;
 
-    private GamePlayer localPlayer;
-
-    public HideAndSeekRoomManager roomManager;
-
-    [SerializeField] private TextMeshProUGUI hider_text;
-    [SerializeField] private TextMeshProUGUI seeker_text;
-
-
-    private void Start()
+    public void UpdatePlayerHealth(int value)
     {
-        roomManager = FindAnyObjectByType<HideAndSeekRoomManager>();
 
-
-        if (roomManager != null)
-        {
-            // 플레이어 수 변경 이벤트에 UI 업데이트 메서드 연결
-            roomManager.OnPlayerCountChanged.AddListener(UpdatePlayerCounts);
-        }
+        hp_text.text = $"{value} ";
         
     }
-
-    private void FindLocalPlayer()
-    {
-        // 현재 씬에 있는 모든 Player 객체를 검색
-        GamePlayer[] players = FindObjectsOfType<GamePlayer>();
-
-        if(players.Length == 0)
-        {
-            Debug.Log("player count is null!");
-        }
-
-
-        foreach (var player in players)
-        {
-            if (player.isLocalPlayer)
-            {
-                Debug.Log("Found Local Player!");
-                localPlayer = player;
-                break;
-            }
-        }
-    }
-
-    private void UpdatePlayerCounts()
-    {
-
-        hider_text.text = $"Hiders : {roomManager.GetTeamCount(1)} ";
-        seeker_text.text = $"Seekers : {roomManager.GetTeamCount(2)}";
-    }
-
 
 }
