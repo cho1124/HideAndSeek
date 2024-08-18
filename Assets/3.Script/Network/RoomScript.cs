af5ad2c79db60a435e82d30f93f908debdbe4210
한유진 <bittersweeety@gmail.com>
2024-08-18 23:43:32 +0900

    [update] if memberis one, ready bttn Active(false)

--- Assets/3.Script/Network/RoomScript.cs
+++ Assets/3.Script/Network/RoomScript.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Mirror;
 using TMPro;
+using UnityEngine.UI; // 버튼을 다루기 위해 추가
 
 public class RoomScript : NetworkBehaviour
 {
     public TMP_Text roomPlayersText;
+    public GameObject readyButtonGameObject; // Button 대신 GameObject로 참조(왠지모르겠는데버튼으로참조가안돼요...ㅡㅡ..)
 
     //private NetworkManager roomManager;
 
@@ -23,10 +25,19 @@ public class RoomScript : NetworkBehaviour
 
     void UpdateRoomText()
     {
-        // ���� �÷��̾� ��
-        
-        var curPlayers = FindObjectsOfType<HideAndSeekRoomPlayer>(); //���̤��Ƥ����Ƥää̤��������Ӥ̤����Ӥ���
+
+        // 현재 플레이어 수 업데이트
+        var curPlayers = FindObjectsOfType<HideAndSeekRoomPlayer>(); 
         roomPlayersText.text = string.Format($"Player: {curPlayers.Length} / 5");
+
+        // 플레이어 수가 1명인 경우 Ready 버튼 비활성화
+        if (curPlayers.Length <= 1) {
+            readyButtonGameObject.SetActive(false);
+        }
+        else {
+            readyButtonGameObject.SetActive(true);
+        }
+
     }
 
 
